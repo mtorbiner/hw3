090a7dd7df30fefc0f4a1b8bec25b30cb4310a8d
mtorbiner <max.torbiner@kellogg.northwestern.edu>
2023-02-21 19:08:37 +0000

    final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
         @post["description"] = params["post"]["description"]
         @post["posted_on"] = params["post"]["posted_on"]
         
-        @post["place_id"] = params["pid"]
+        @post["place_id"] = params["post"]["place_id"]
         
         
         @post.save
